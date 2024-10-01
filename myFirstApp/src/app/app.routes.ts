aff1bc4fda727bafcc792f55be6f230b7ad1bc5f
Hristina <hristinastankova89@gmail.com>
2024-10-01 15:12:31 +0200

    adding template driven form on branch 6

--- myFirstApp/src/app/app.routes.ts
+++ myFirstApp/src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ArticleComponentComponent } from './article-component/article-component.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 export const routes: Routes = [
     // { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: '', component: HomePageComponent },
     { path: 'article/:id', component: ArticleComponentComponent },
+    { path: 'contact', component: ContactFormComponent },
+
     { path: '**', component: NotFoundComponent }
 ];
---
+++ myFirstApp/src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, NgModule } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Message } from '../message.model';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss'
+})
+export class ContactFormComponent {
+
+  newMessage: Message = {
+    lastname: "",
+    firstname: "",
+    email: "",
+    content: ""
+  };
+  onSubmit(): void {
+    console.log('Formulaire envoyé !');
+  }
+}
